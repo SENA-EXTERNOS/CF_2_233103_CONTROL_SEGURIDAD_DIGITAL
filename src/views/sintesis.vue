7bb49793a20bfbc0819451f71b555ef4fe79c152
paolaandrear <paoramos1999@gmail.com>
2022-11-01 18:17:12 -0500

    Se envia ajuste de la sintesis

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -31,7 +31,7 @@
     .row.justify-content-center
         .col-lg-3.sm-8.mt-5  
           .col-auto
-            a.anexo.mb-4(:href="obtenerLink('/downloads/CF2 Sintesis.pdf')" target="_blank")
+            a.anexo.mb-4(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
               .anexo__icono
                 img(src="@/assets/template/icono-pdf.svg")
               .anexo__texto
