6d0d07ff3ffd2a6d771308c69fc5295d67fc3766
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-02-02 18:16:16 -0500

    Se vinculan los videos.

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -158,7 +158,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Dc7PGh5Aiss" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       
     h3.titulo-tercero(data-aos='fade-right') Técnicas de diagnóstico
 
